5a4fc9b3f2a4b8e4009cc589b0f238dedd567e5b
Chelsea Zhu <qianwei5668@gmail.com>
2018-11-13 11:55:19 -0800

    get new data from API and save to MongoDB

---
+++ static/functions.py
@@ -0,0 +1,24 @@
+from browser import document, ajax
+import json
+
+
+def on_complete(req):
+    if req.status==200 or req.status==0:
+        document["result"].html = req.text
+    else:
+        document["result"].html = "error " + req.text
+
+def get_prediction(ev):
+    """Get the predicted probability."""
+    req = ajax.ajax()
+    req.bind('complete', on_complete)
+    req.open('POST', '/predict', True)
+    req.set_header('content-type','application/json')
+    data = json.dumps({'user_input': document['user_input'].value})
+    req.send(data)
+
+document["predict_button"].bind("click", get_prediction)
+
+
+
+
